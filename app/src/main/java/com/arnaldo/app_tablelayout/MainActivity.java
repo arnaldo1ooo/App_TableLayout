8006ddf21f0e9d04d42db8fc283acf44de4a649a
arnaldo1ooo <arnaldo1ooo@hotmail.com>
2019-09-16 17:42:00 -0400

    Initial commit

---
+++ app/src/main/java/com/arnaldo/app_tablelayout/MainActivity.java
@@ -0,0 +1,17 @@
+package com.arnaldo.app_tablelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
